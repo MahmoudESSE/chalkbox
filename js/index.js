1bc80e713716998a4d7629d152b3b96b6b767255
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-02-09 16:48:55 +0100

    feat: add clear button
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,12 +1,37 @@
-/**
- * @type {Array<HTMLDivElement>} boxes
- */
-
 // create a 16x16 grid
 // we create 16 rows
 // each row has 16 columns
 
 const color = document.getElementById("color");
+
+const grid = document.getElementById("grid");
+grid.className = "grid";
+
+const sizeBtn = document.getElementById("size");
+let size = sizeBtn.value;
+
+const clearBtn = document.getElementById("clear");
+
+clearBtn.addEventListener("click", () => {
+  clearGrid(grid);
+  createGrid(grid, size);
+});
+
+sizeBtn.addEventListener(
+  "change",
+  (e) => {
+    size = e.target.value;
+    if (size > 100) {
+      size = 99;
+    }
+    if (size <= 0) {
+      size = 1;
+    }
+    clearGrid(grid);
+    createGrid(grid, size);
+  },
+  false,
+);
 
 /**
  * @param {HTMLDivElement} grid
@@ -18,18 +43,31 @@ function createGrid(grid, size) {
 
     pixel.className = "pixel";
     pixel.id = `pixel-${i}`;
-    pixel.style.width = 600 / 16 + "px";
-    pixel.style.height = 600 / 16 + "px";
+    pixel.style.width = 600 / size + "px";
+    pixel.style.height = 600 / size + "px";
 
     grid.appendChild(pixel);
 
-    pixel.addEventListener("mouseover", (e) => {
-      e.target.style.backgroundColor = color.value;
-    });
+    pixel.addEventListener(
+      "mouseover",
+      (e) => {
+        e.target.style.backgroundColor = color.value;
+      },
+      false,
+    );
   }
 }
 
-const grid = document.getElementById("grid");
-grid.className = "grid";
+/**
+ * @param {HTMLDivElement} grid
+ */
+function clearGrid(grid) {
+  let length = grid.children.length;
+  let children = grid.children;
 
-createGrid(grid, 16);
+  for (let i = 0; i < length; i++) {
+    grid.removeChild(children[0]);
+  }
+}
+
+createGrid(grid, size);
